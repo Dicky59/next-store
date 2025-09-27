782af44b31e3d47f57e6f84294c83e075d29b83d
Dicky59 <pekkaarvolaine@gmail.com>
2025-09-27 18:34:16 +0300

    cart zod schema & prisma model

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod'
-import { insertProductSchema } from '../lib/validators'
+import {
+  cartItemSchema,
+  insertCartSchema,
+  insertProductSchema,
+} from '../lib/validators'
 
 // Type for inserting products
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -7,3 +11,7 @@ export type Product = z.infer<typeof insertProductSchema> & {
   rating: string // Decimal from Prisma becomes string when serialized
   createdAt: Date
 }
+
+export type Cart = z.infer<typeof insertCartSchema>
+
+export type CartItem = z.infer<typeof cartItemSchema>
