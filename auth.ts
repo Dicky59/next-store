a78b63fcf6fb15d491e58608d2dd41579b117425
Dicky59 <pekkaarvolaine@gmail.com>
2025-09-29 16:04:33 +0300

    persist session cart

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { compareSync } from 'bcrypt-ts-edge'
 import type { NextAuthConfig } from 'next-auth'
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { cookies } from 'next/headers'
 
 export const config = {
   pages: {
@@ -67,9 +68,10 @@ export const config = {
 
       return session
     },
-    async jwt({ token, user, trigger, session }: any) {
+    async jwt({ token, user, trigger, session }) {
       // Assign user fields to token
       if (user) {
+        token.id = user.id
         token.role = user.role
 
         // If user has no name then use the email
@@ -81,6 +83,29 @@ export const config = {
             where: { id: user.id },
             data: { name: token.name },
           })
+        }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies()
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            })
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              })
+
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              })
+            }
+          }
         }
       }
       return token
