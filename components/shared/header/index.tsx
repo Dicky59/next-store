0276feb5a6a69072dd2f2d696ede48af579a0759
Dicky59 <pekkaarvolaine@gmail.com>
2025-11-03 23:23:58 +0200

    category drawer

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,22 +1,24 @@
-import { APP_NAME } from '@/lib/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import Menu from './menu'
+import { APP_NAME } from '@/lib/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import CategoryDrawer from './category-drawer';
+import Menu from './menu';
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
-      <div className="wrapper flex-between">
-        <div className="flex-start">
-          <Link href="/" className="flex-start">
+    <header className='w-full border-b'>
+      <div className='wrapper flex-between'>
+        <div className='flex-start'>
+          <CategoryDrawer />
+          <Link href='/' className='flex-start ml-4'>
             <Image
-              src="/images/logo.svg"
+              src='/images/logo.svg'
               alt={`${APP_NAME} logo`}
-              width={48}
               height={48}
+              width={48}
               priority={true}
             />
-            <span className="hidden lg:block font-bold text-2xl ml-3">
+            <span className='hidden lg:block font-bold text-2xl ml-3'>
               {APP_NAME}
             </span>
           </Link>
@@ -24,7 +26,7 @@ const Header = () => {
         <Menu />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
